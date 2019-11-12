fc44f67ecae33f9c6843131791f865e275b022c3
vy vo <tuonggvy.vo@gmail.com>
2019-11-12 02:00:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+
+def prime?(n)
+  if n <= 1
+    return false
+  elsif
+    (2..n-1).each do |i|
+      if n%i == 0
+        return false
+      end
+    end
+  else
+    return true
+  end
+end
