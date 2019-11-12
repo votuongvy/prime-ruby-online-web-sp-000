056207683a1e1a1dbe263945aed90b4dedf82fad
vy vo <tuonggvy.vo@gmail.com>
2019-11-12 02:02:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+
+def prime?(n)
+  if n <= 1
+    return false
+  elsif
+    (2..n-1).each do |i|
+      if n%i == 0
+        return false
+      end
+    end
+  end
+  return true #def prime? is true unless it go through if to return false
+end
